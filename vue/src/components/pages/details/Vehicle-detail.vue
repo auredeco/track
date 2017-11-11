a1e1629519bb5f09ef3fb8b7a5609dc512393b0e
Aurelio Decock <auredeco@student.arteveldehs.be>
2017-11-11 15:52:50 +0100

    added relational get request

--- vue/src/components/pages/details/Vehicle-detail.vue
+++ vue/src/components/pages/details/Vehicle-detail.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="block">
-    <h1>{{ vehicle.name[0].value }}</h1>
+  <div class="block" v-cloak>
+    <h1 v-if='vehicle'>{{ vehicle.name[0].value }}</h1>
+    <img v-if="vehicle" :src="vehicle.field_image[0].url" v-bind:alt="vehicle.field_image[0].alt">
+    <p v-if="owner">{{ owner.name[0].value }}</p>
   </div>
 </template>
 
@@ -10,7 +12,8 @@ export default {
   name: 'VehicleDetail',
   data () {
     return {
-      vehicle: []
+      vehicle: null,
+      owner: null
     }
   },
 
@@ -20,18 +23,34 @@ export default {
         let uri = this.$parent.uri;
 
         axios
-        .get( uri + 'track/vehicle/' + id + '?_format=hal_json')
+        .get( uri + 'track/vehicle/' + id + '?_format=json')
         .then(response => {
-        // JSON responses are automatically parsed.
-        self.vehicle = response.data;
+          // JSON responses are automatically parsed.
+          self.vehicle = response.data;
+          this.getVehicleOwner();
         })
         .catch(e => {
-        this.errors.push(e)
+          this.errors.push(e)
+        });
+        
+    },
+    getVehicleOwner() {
+      let self = this;
+      let uri = this.$parent.uri;
+      let ownerId = this.vehicle.field_owner[0].target_id
+
+      axios
+        .get( uri + 'user/' + ownerId + '?_format=json')
+        .then(response => {
+          self.owner = response.data
         })
+        .catch(e => {
+          this.errors.push(e)
+        });
     }
   },
-  created () {
-      this.getVehicleById(this.$route.params.id)
+  mounted () {
+      this.getVehicleById(this.$route.params.id);
   }
 }
 </script>
